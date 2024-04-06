19f33af24e12c9a04818477eba88f409fd73c368
HeidyPrieto <140523540+HeidyPrieto@users.noreply.github.com>
2024-04-05 21:21:50 -0500

    (Backend): Creating users from the app

--- AppReactNative/src/Data/Sources/remote/api/ApiDelivery.tsx
+++ AppReactNative/src/Data/Sources/remote/api/ApiDelivery.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const ApiDelivery = axios.create({
-    baseURL: 'https://10.175.80.246:3000/api',
+    baseURL: 'http://10.175.80.246:3000/api',
     headers: {
         'Content-Type': 'application/json'
     }
